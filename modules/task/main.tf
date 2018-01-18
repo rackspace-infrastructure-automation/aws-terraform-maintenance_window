787842de4c54a799adb3f022971ff060bd9e0f7e
twistedgrim <rebel623@gmail.com>
2018-01-18 09:36:15 -0500

    Docker CI update and fixes

--- modules/task/main.tf
+++ modules/task/main.tf
@@ -1,3 +1,37 @@
+/**
+* # aws-terraform-maintenance_window/modules/task
+*
+*This submodule creates an Maintenance Window Task
+*
+*## Basic Usage
+*
+*```
+*module "maintenance_window_task_1" {
+*  source           = "git@github.com:rackspace-infrastructure-automation/aws-terraform-maintenance_window//modules/task?ref=v0.0.1"
+*  max_errors       = "1"
+*  service_role_arn = "arn:aws:iam::794790922771:role/aws-service-role/ssm.amazonaws.com/AWSServiceRoleForAmazonSSM"
+*  priority         = "0"
+*  task_type        = "RUN_COMMAND"
+*  task_arn         = "arn:aws:ssm:${data.aws_region.current_region.name}:507897595701:document/Rack-ConfigureAWSTimeSync"
+*  window_id        = "${module.maint_window_target.maintenance_window_id}"
+*  max_concurrency  = "5"
+*  target_key       = "WindowTargetIds"
+*  target_values    = ["${module.maint_window_target.maintenance_window_target_id}"]
+*
+*  task_parameters = {
+*    name   = "PreferredTimeClient"
+*    values = ["chrony"]
+*  }
+*
+*  enable_s3_logging = true
+*  s3_bucket_name    = "${module.s3_logging.bucket_id}"
+*  s3_region         = "${module.s3_logging.bucket_region}"
+*}
+*```
+*
+* Full working references are available at [examples](examples)
+*/
+
 resource "aws_ssm_maintenance_window_task" "maintenance_window_task_with_logging" {
   count            = "${var.enable_s3_logging ? 1 : 0}"
   name             = "${var.resource_name}"
